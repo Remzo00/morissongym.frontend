3d9d8cf6d60340ccc1a66095ef7e2d8a9e85a37a
izudinmavric <izudin.mavric@gmail.com>
2022-05-26 14:29:53 +0200

    themecolorsadd

---
+++ src/assets/theme/index.d.ts
@@ -0,0 +1,10 @@
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      white: string;
+      black: string;
+      primary: string;
+      gray: string;
+    };
+  }
+}
