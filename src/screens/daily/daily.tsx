ea443e5ce459663ddf01680c41663a0e68a66d73
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-09-13 15:53:27 +0200

    fixing daily

--- src/screens/daily/daily.tsx
+++ src/screens/daily/daily.tsx
@@ -1,14 +1,16 @@
 import Header from "../../components/header/header";
 import Navbar from "../../components/navbar";
 import {
-  Wrapper,
-  Main,
-  Services,
-  TrainingImg,
-  DrinksImg,
-  Foodimg,
-  Supplementsimg,
-  ImgWrapper,
+    Wrapper,
+    Main,
+    Services,
+    TrainingImg,
+    DrinksImg,
+    Foodimg,
+    Supplementsimg,
+    ImgWrapper,
+    HeadingWrapper,
+    Heading,
 } from "./daily.styled";
 import supplements from "../../assets/images/supplements.png";
 import training from "../../assets/images/training.png";
@@ -16,24 +18,36 @@ import drinks from "../../assets/images/drinks.png";
 import food from "../../assets/images/food.png";
 
 const Daily = () => {
-  return (
-    <Main>
-      <Header />
-      <Wrapper>
-        <Navbar />
-        <ImgWrapper>
-          <Services>
-            <TrainingImg src={training} />
-            <Foodimg src={food} />
-          </Services>
-          <Services>
-            <DrinksImg src={drinks} />
-            <Supplementsimg src={supplements} />
-          </Services>
-        </ImgWrapper>
-      </Wrapper>
-    </Main>
-  );
+    return (
+        <Main>
+            <Header />
+            <Wrapper>
+                <Navbar />
+                <ImgWrapper>
+                    <Services>
+                        <HeadingWrapper>
+                            <Heading>Training</Heading>
+                            <TrainingImg src={training} />
+                        </HeadingWrapper>
+                        <HeadingWrapper>
+                            <Heading>Suplements</Heading>
+                            <Supplementsimg src={supplements} />
+                        </HeadingWrapper>
+                    </Services>
+                    <Services>
+                        <HeadingWrapper>
+                            <Heading>Drinks</Heading>
+                            <DrinksImg src={drinks} />
+                        </HeadingWrapper>
+                        <HeadingWrapper>
+                            <Heading>Food</Heading>
+                            <Foodimg src={food} />
+                        </HeadingWrapper>
+                    </Services>
+                </ImgWrapper>
+            </Wrapper>
+        </Main>
+    );
 };
 
 export default Daily;
