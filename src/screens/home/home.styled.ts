5e7681f3fc4ab1782fbd63a074eb70a33700079f
Hamza Gorcevic <88393813+HamzaGorcevic@users.noreply.github.com>
2022-09-14 19:13:30 +0200

    Changed first two home cards

--- src/screens/home/home.styled.ts
+++ src/screens/home/home.styled.ts
@@ -1,5 +1,96 @@
 import styled from "styled-components";
 
+interface Props {
+  background?: string;
+  height?: number;
+}
 export const Wrapper = styled.div`
-    display: flex;
-`
+  display: flex;
+`;
+
+export const HomeContainer = styled.div`
+  padding-left: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: black;
+  align-items: center;
+`;
+export const WrapCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const CardContainer = styled.div<Props>`
+  position: relative;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+
+  width: 500px;
+  height: 300px;
+  margin: 20px;
+  background: ${(props) => props.background && props.background};
+`;
+export const CardTitle = styled.h1`
+  color: white;
+  font-size: 22px;
+  font-weight: 700;
+  margin-left: 30px;
+`;
+export const CardGymTime = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+`;
+export const GymNow = styled.div`
+  color: white;
+  width: 49%;
+  border-radius: 20px;
+  background-color: rgba(50, 49, 49, 0.636);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ProgressCircle = styled.div`
+  width: 80px;
+  height: 80px;
+`;
+
+export const CardP = styled.p`
+  font-size: large;
+  color: white;
+  margin-left: 10px;
+`;
+
+export const CardOverview = styled.div`
+  width: 90%;
+  height: 80%;
+  display: flex;
+  padding: 10px;
+  justify-content: space-around;
+  margin: auto;
+`;
+
+export const DashContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  width: 10%;
+  height: 100%;
+`;
+
+export const Dash = styled.div<Props>`
+  width: 100%;
+  height: ${(props) => `${props.height}%`};
+
+  background: ${(props) =>
+    props.background ? props.background : "rgba(126, 205, 22, 0.215)"};
+  border-radius: 10px 10px 0px 0px;
+`;
+
+export const DashText = styled.p`
+  color: white;
+`;
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,18 +1,119 @@
+import { BackgroundImage } from "@mantine/core";
 import React from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import Header from "../../components/header/header";
 import Navbar from "../../components/navbar";
-import { Wrapper } from "./home.styled";
+
+import {
+  CardContainer,
+  Wrapper,
+  HomeContainer,
+  CardTitle,
+  CardGymTime,
+  GymNow,
+  ProgressCircle,
+  CardP,
+  CardOverview,
+  WrapCardContainer,
+  DashContainer,
+  Dash,
+  DashText,
+} from "./home.styled";
 
 const Home = () => {
-    return(
-        <>
-         <Header />
-        <Wrapper>       
-            <Navbar />
-            HOME
-        </Wrapper>           
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <Wrapper>
+        <Navbar />
+        <HomeContainer>
+          <WrapCardContainer>
+            <CardTitle>Activity</CardTitle>
+            <CardContainer>
+              <CardGymTime>
+                <GymNow>
+                  <CardP>In the gym:</CardP>
+                  <h1>24</h1>
+                  <ProgressCircle>
+                    <CircularProgressbar
+                      maxValue={50}
+                      value={22}
+                      strokeWidth={10}
+                      styles={buildStyles({
+                        pathColor: `#7DCD16`,
+                        trailColor: "#626262",
+                      })}
+                    />
+                  </ProgressCircle>
+                </GymNow>
+                <GymNow>
+                  <CardP>Average:</CardP>
+                  <h1>24</h1>
+                  <ProgressCircle>
+                    <CircularProgressbar
+                      maxValue={50}
+                      value={42}
+                      strokeWidth={10}
+                      styles={buildStyles({
+                        pathColor: `#7DCD16`,
+                        trailColor: "#626262",
+                      })}
+                    />
+                  </ProgressCircle>
+                </GymNow>
+              </CardGymTime>
+            </CardContainer>
+          </WrapCardContainer>
 
-export default Home
+          <WrapCardContainer>
+            <CardTitle>Overview</CardTitle>
+            <CardContainer background={"rgba(50, 49, 49, 0.636)"}>
+              <CardP>Tracking history:</CardP>
+              <CardOverview>
+                <DashContainer>
+                  <Dash height={50}></Dash>
+
+                  <DashText>Mon</DashText>
+                </DashContainer>
+                <DashContainer>
+                  <Dash height={30}></Dash>
+                  <DashText>Tue</DashText>
+                </DashContainer>
+                <DashContainer>
+                  <Dash height={50}></Dash>
+
+                  <DashText>Wen</DashText>
+                </DashContainer>
+                <DashContainer>
+                  <Dash height={50}></Dash>
+                  <DashText>Thu</DashText>
+                </DashContainer>
+                <DashContainer>
+                  <Dash height={20}></Dash>
+                  <DashText>Fri</DashText>
+                </DashContainer>
+                <DashContainer>
+                  <Dash background="#8aff42" height={100}></Dash>
+                  <DashText>Sat</DashText>
+                </DashContainer>
+                <DashContainer>
+                  <Dash height={50}></Dash>
+                  <DashText>Sun</DashText>
+                </DashContainer>
+              </CardOverview>
+            </CardContainer>
+          </WrapCardContainer>
+          <CardContainer>
+            <CardTitle>Summary</CardTitle>
+          </CardContainer>
+          <CardContainer>
+            <CardTitle>Chart</CardTitle>
+          </CardContainer>
+        </HomeContainer>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Home;
