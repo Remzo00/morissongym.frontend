7031d298451ada6d91bea5472d73060d292bbd90
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-09-22 16:36:17 +0200

    Hot fix

--- src/components/navbar/NavbarData.tsx
+++ src/components/navbar/NavbarData.tsx
@@ -8,8 +8,9 @@ import {
 
 export const data = [
     { link: "/", label: "HOME", icon: IconHome2 },
-    { link: "rfid", label: "RFID", icon: IconUser },
     { link: "daily", label: "DAILY", icon: IconClipboardList },
     { link: "members", label: "MEMBERS", icon: IconUsers },
+    { link: "coachs", label: "Coach", icon: IconUsers },
+
     { link: "settings", label: "SETTINGS", icon: IconSettings },
 ];
