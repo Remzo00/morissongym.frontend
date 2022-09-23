8e153d386cd13ca312e6166e8a75a4d9ce9f0d16
Remzo00 <gusinacremzo00@gmail.com>
2022-09-23 16:03:57 +0200

    hot fix

--- src/components/navbar/NavbarData.tsx
+++ src/components/navbar/NavbarData.tsx
@@ -1,15 +1,16 @@
 import {
-    IconHome2,
-    IconUser,
-    IconClipboardList,
-    IconUsers,
-    IconSettings,
+  IconHome2,
+  IconUser,
+  IconClipboardList,
+  IconUsers,
+  IconSettings,
 } from "@tabler/icons";
 
 export const data = [
-    { link: "/", label: "HOME", icon: IconHome2 },
-    { link: "rfid", label: "RFID", icon: IconUser },
-    { link: "daily", label: "DAILY", icon: IconClipboardList },
-    { link: "members", label: "MEMBERS", icon: IconUsers },
-    { link: "settings", label: "SETTINGS", icon: IconSettings },
+  { link: "/", label: "HOME", icon: IconHome2 },
+  { link: "rfid", label: "RFID", icon: IconUser },
+  { link: "daily", label: "DAILY", icon: IconClipboardList },
+  { link: "members", label: "MEMBERS", icon: IconUsers },
+  { link: "coachs", label: "COACHS", icon: IconUsers },
+  { link: "settings", label: "SETTINGS", icon: IconSettings },
 ];
