5b9dca5083c1873a5e29896c38fe91a6726cfd4f
Remzo00 <gusinacremzo00@gmail.com>
2022-10-07 15:54:57 +0200

    Add validation on edit buttons

--- src/components/EditMemberDialog/EditMemberDialog.tsx
+++ src/components/EditMemberDialog/EditMemberDialog.tsx
@@ -1,4 +1,4 @@
-import { Modal } from "@mantine/core";
+import { Modal, TextInput } from "@mantine/core";
 import React, { useState } from "react";
 import api from "../../api";
 import { Customer } from "../../types/types";
@@ -12,6 +12,8 @@ import {
   DeleteButton,
   ConfirmButton,
 } from "./EditMemberDialog.style";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 //TODO
 //Finish implementing value on input for startDate and endDate
@@ -24,6 +26,22 @@ type Props = {
 
 const editMemberDialog = ({ opened, setOpened, customer }: Props) => {
   const [membersValues, setMembersValues] = useState(customer);
+
+  const EditMemberSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    lastName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    phoneNumber: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+  });
 
   const deleteMember = (id: number) => {
     api.deleteCoach(id);
@@ -54,41 +72,107 @@ const editMemberDialog = ({ opened, setOpened, customer }: Props) => {
       >
         <Title>Edit Member</Title>
         <Wrapper>
-          <Text>First Name</Text>
-          <Input
-            onChange={(e) =>
-              setMembersValues({ ...membersValues, firstName: e.target.value })
-            }
-            value={membersValues.firstName}
-          />
-          <Text>Last Name</Text>
-          <Input
-            onChange={(e) =>
-              setMembersValues({ ...membersValues, lastName: e.target.value })
-            }
-            value={membersValues.lastName}
-          />
-          <Text>Contact</Text>
-          <Input
-            onChange={(e) =>
-              setMembersValues({
-                ...membersValues,
-                phoneNumber: e.target.value,
-              })
-            }
-            value={membersValues.phoneNumber}
-          />
-          <Text>Email</Text>
-          <Input
-            onChange={(e) =>
-              setMembersValues({ ...membersValues, email: e.target.value })
-            }
-            value={membersValues.email}
-          />
-          <Text>Start Date</Text>
-          <Input />
-          <Text>End Date</Text>
-          <Input />
+          <Formik
+            initialValues={{
+              firstName: "",
+              lastName: "",
+              email: "",
+              phoneNumber: "",
+            }}
+            validationSchema={EditMemberSchema}
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <Text>First Name</Text>
+                <Field name="firstName">
+                  {({ field, form: { touched, errors }, meta }: any) => (
+                    <div>
+                      <TextInput
+                        type="text"
+                        onChange={(e) =>
+                          setMembersValues({
+                            ...membersValues,
+                            firstName: e.target.value,
+                          })
+                        }
+                        value={membersValues.firstName}
+                      />
+                      {meta.touched && meta.error && (
+                        <div className="error">{meta.error}</div>
+                      )}
+                    </div>
+                  )}
+                </Field>
+                <Text>Last Name</Text>
+                <Field name="lastName">
+                  {({ field, form: { touched, errors }, meta }: any) => (
+                    <div>
+                      <TextInput
+                        type="text"
+                        onChange={(e) =>
+                          setMembersValues({
+                            ...membersValues,
+                            lastName: e.target.value,
+                          })
+                        }
+                        value={membersValues.lastName}
+                      />
+                      {meta.touched && meta.error && (
+                        <div className="error">{meta.error}</div>
+                      )}
+                    </div>
+                  )}
+                </Field>
+                <Text>Contact</Text>
+                <Field name="phoneNumber">
+                  {({ field, form: { touched, errors }, meta }: any) => (
+                    <div>
+                      <TextInput
+                        type="text"
+                        onChange={(e) =>
+                          setMembersValues({
+                            ...membersValues,
+                            phoneNumber: e.target.value,
+                          })
+                        }
+                        value={membersValues.phoneNumber}
+                      />
+                      {meta.touched && meta.error && (
+                        <div className="error">{meta.error}</div>
+                      )}
+                    </div>
+                  )}
+                </Field>
+                <Text>Email</Text>
+                <Field name="email">
+                  {({ field, form: { touched, errors }, meta }: any) => (
+                    <div>
+                      <TextInput
+                        type="text"
+                        onChange={(e) =>
+                          setMembersValues({
+                            ...membersValues,
+                            email: e.target.value,
+                          })
+                        }
+                        value={membersValues.email}
+                      />
+                      {meta.touched && meta.error && (
+                        <div className="error">{meta.error}</div>
+                      )}
+                    </div>
+                  )}
+                </Field>
+                <Text>Start Date</Text>
+                <Input />
+                <Text>End Date</Text>
+                <Input />
+              </Form>
+            )}
+          </Formik>
         </Wrapper>
         <ButtonDiv>
           <DeleteButton onClick={() => deleteMember(membersValues.id)}>
