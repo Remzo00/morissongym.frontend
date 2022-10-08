42c2028be8d883c9dc22b71563cd62fee8072a30
Hamza Gorcevic <88393813+HamzaGorcevic@users.noreply.github.com>
2022-10-08 19:26:21 +0200

    Setting started

--- src/screens/settings/settings.styled.ts
+++ src/screens/settings/settings.styled.ts
@@ -1,5 +1,48 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    display: flex;
-`
+  background-color: black;
+  height: 100vh;
+  padding: 30px;
+  width: 100%;
+  & > input {
+    background: black;
+  }
+`;
+
+export const TitleSetting = styled.div`
+  font-size: 20px;
+  font-weight: 900;
+  letter-spacing: 1px;
+  color: white;
+  margin-bottom: 15px;
+`;
+
+export const WrapperSettings = styled.div`
+  background-color: #1a1b1e;
+  height: auto;
+  padding: 30px;
+  border-radius: 10px;
+`;
+
+export const SettingElement = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SettingButton = styled.button`
+  width: 100px;
+  height: 30px;
+  border: 2px solid white;
+  border-radius: 10px;
+  color: white;
+  background: none;
+  transition: all 1s ease;
+  display: block;
+  margin-top: 10px;
+  margin-left: auto;
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+`;
--- src/screens/settings/settings.tsx
+++ src/screens/settings/settings.tsx
@@ -1,12 +1,61 @@
-import React from "react";
-import Header from "../../components/header/header";
-import Navbar from "../../components/navbar";
-import { Wrapper } from "./settings.styled";
+import React, { useContext } from "react";
+import { useColorScheme } from "@mantine/hooks";
+
+import {
+  SettingElement,
+  TitleSetting,
+  Wrapper,
+  WrapperSettings,
+  SettingButton,
+} from "./settings.styled";
+import {
+  ColorSchemeProvider,
+  Select,
+  useMantineColorScheme,
+} from "@mantine/core";
 
 const Settings = () => {
   return (
     <>
-      <Wrapper>SETTINGS</Wrapper>
+      <Wrapper>
+        <TitleSetting>Settings</TitleSetting>
+        <WrapperSettings>
+          <SettingElement>
+            <TitleSetting>Language</TitleSetting>
+            <Select
+              style={{ borderRadius: "10px", color: "white" }}
+              variant="unstyled"
+              placeholder="Chose your language"
+              defaultValue={"english"}
+              icon=""
+              data={[
+                { value: "english", label: "English" },
+                { value: "serbian", label: "Serbian" },
+                { value: "germany", label: "Germany" },
+              ]}
+              styles={() => ({
+                item: {
+                  backgroundColor: "grey",
+
+                  // applies styles to selected item
+                },
+                dropdown: {
+                  background: "none",
+                  border: "none",
+                },
+                input: {
+                  border: "1px solid #606060",
+                  borderRadius: "10px",
+                  color: "white",
+                  fontSize: "15px",
+                  padding: "20px",
+                },
+              })}
+            />
+          </SettingElement>
+          <SettingButton>Button</SettingButton>
+        </WrapperSettings>
+      </Wrapper>
     </>
   );
 };
